2da0205fc2359eabdfd8bebdcfea3b0d9b79c0d5
Christoph Krey <c@ckrey.de>
2017-11-22 08:20:43 +0100

    [FIX] change debug level for partially received messages

--- MQTTClient/MQTTClient/MQTTDecoder.m
+++ MQTTClient/MQTTClient/MQTTDecoder.m
@@ -131,8 +131,9 @@
                 self.dataBuffer = nil;
                 self.state = MQTTDecoderStateDecodingHeader;
             } else {
-                DDLogError(@"[MQTTDecoder] oops received (%lu)=%@...", (unsigned long)self.dataBuffer.length,
-                             [self.dataBuffer subdataWithRange:NSMakeRange(0, MIN(256, self.dataBuffer.length))]);
+                DDLogWarn(@"[MQTTDecoder] oops received (%lu)=%@...",
+                          (unsigned long)self.dataBuffer.length,
+                          [self.dataBuffer subdataWithRange:NSMakeRange(0, MIN(256, self.dataBuffer.length))]);
             }
         }
     }
